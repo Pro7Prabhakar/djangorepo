11f79b69764cbc1f7c8e776913e510ff69747756
Prabhashankar <124426683+Pro7Prabhakar@users.noreply.github.com>
2023-12-27 01:02:51 +0530

    Update views.py

--- firstproject/firstapp/views.py
+++ firstproject/firstapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def display(req):
-    s = '<h1>Hello Students welcome to Mahesh sir Django Classes</h1>'
+    s = '<h1>Hello Everyone!!!</h1>'
     return HttpResponse(s)
